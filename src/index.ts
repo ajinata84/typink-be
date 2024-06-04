660753881c8b26c582b67c58816eff817b7b95a9
ajinata84 <ajinata89@gmail.com>
2024-06-04 18:25:06 +0700

    gh action trigger

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ app.use(cors());
 app.use(helmet());
 app.use(express.json());
 
-app.use("/auth", authRoutes);
+app.use("/auth", authRoutes); 
 
 app.use("/collection", collectionRoutes);
 app.use("/donations", donationRoutes);
